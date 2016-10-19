c2ee027f9829d8a67f5761eb72f7f5bc768c8cce
louisbl <louis@beltramo.me>
2016-10-19 19:07:33 +0200

    Add click listener on button

--- app/src/main/java/ddi/gobelins/hello/MainActivity.java
+++ app/src/main/java/ddi/gobelins/hello/MainActivity.java
@@ -2,6 +2,9 @@ package ddi.gobelins.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MainActivity", "on main button clicked");
+            }
+        });
     }
 }
